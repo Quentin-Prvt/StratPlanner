3896b47eecdf9d0f7fd77f68a9873bf77ebf0e66
Quentin PRUVOT <quentin.pruvot@student.junia.com>
2026-01-02 21:40:39 +0100

    fix bug

--- src/utils/mapsRegistry.ts
+++ src/utils/mapsRegistry.ts
@@ -1,18 +1,18 @@
 // src/utils/mapsRegistry.ts
 
 // Importation de toutes les images
-import ascentMap from '../assets/maps/Ascent.svg';
-import bindMap from '../assets/maps/Bind.svg';
-import breezeMap from '../assets/maps/Breeze.svg';
-import corrodeMap from '../assets/maps/Corrode.svg';
-import fractureMap from '../assets/maps/Fracture.svg';
-import havenMap from '../assets/maps/Haven.svg';
-import iceboxMap from '../assets/maps/Icebox.svg';
-import lotusMap from '../assets/maps/Lotus.svg';
-import pearlMap from '../assets/maps/Pearl.svg';
-import splitMap from '../assets/maps/Split.svg';
-import sunsetMap from '../assets/maps/Sunset.svg';
-import abyssMap from '../assets/maps/Abyss.svg';
+import ascentMap from '../assets/maps/ascent.svg';
+import bindMap from '../assets/maps/bind.svg';
+import breezeMap from '../assets/maps/breeze.svg';
+import corrodeMap from '../assets/maps/corrode.svg';
+import fractureMap from '../assets/maps/fracture.svg';
+import havenMap from '../assets/maps/haven.svg';
+import iceboxMap from '../assets/maps/icebox.svg';
+import lotusMap from '../assets/maps/lotus.svg';
+import pearlMap from '../assets/maps/pearl.svg';
+import splitMap from '../assets/maps/split.svg';
+import sunsetMap from '../assets/maps/sunset.svg';
+import abyssMap from '../assets/maps/abyss.svg';
 
 // Définition du type pour l'objet Map
 export const MAPS_REGISTRY: Record<string, string> = {
